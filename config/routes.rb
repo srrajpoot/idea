66a3c827cd2f9bba1de850c1a1692f877ae71678
srrajpoot <srrajpoot01@gmail.com>
2024-04-22 17:53:34 +0530

    [123456] added route file

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,23 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
 
+post '/fedbacks_create', to: 'feedbacks#create'
+post '/fedbacks', to: 'feedbacks#create'
+
+
   resources :accounts do
     post :resources, on: :collection
   end 
 
   resources :logins
+  resources :addresses, only: [:create]
+  resources :feedbacks, only: [:create, :update, :destroy]
+  resources :settings, only: [:index, :show, :create, :update, :destroy]
+  resources :add_ideas, only: [:index,:create, :show, :update, :destroy]
+  resources :likes, only: [:index, :show, :create, :update, :destroy]
+  resources :posts, only: [:index, :show, :create, :update, :destroy]
 
-  # namespace :logins do
-  #   namespace :v1 do
-  #     post '/login', to: 'logins#create'
-  #   end
-  # end
+
+
 
 end
